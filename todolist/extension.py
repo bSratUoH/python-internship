a80dbe88e51686f4d6f900d1d4c811cd6471a659
bsingh <barun.singh@cyberark.com>
2024-07-18 17:14:16 +0530

    📝 Add comment
    A comment added in the extension file

--- todolist/extension.py
+++ todolist/extension.py
@@ -1,5 +1,6 @@
 # this file contains the initialization of the ORM object
 # which is used to interact with the database
+# the ORM object is created using the SQLAlchemy library
 
 from flask_sqlalchemy import SQLAlchemy
 
