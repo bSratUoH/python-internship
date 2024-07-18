477b5dceb3323c0aff3b94c40b9773a768828702
bsingh <barun.singh@cyberark.com>
2024-07-18 08:35:40 +0530

    added test case sample

--- oop_assignments/bank_management.py
+++ oop_assignments/bank_management.py
@@ -22,12 +22,43 @@ class BankAccount:
         return self.balance
 
 
-account1 = BankAccount("123456789", "Alice")
-print(f"Initial balance: ${account1.get_balance()}")
+# account1 = BankAccount("123456789", "Alice")
+# print(f"Initial balance: ${account1.get_balance()}")
 
-account1.deposit(1000)
-account1.withdraw(400)
-account1.withdraw(800)  # This withdrawal will fail due to insufficient funds
-print(f"Final balance: ${account1.get_balance()}")
-# hjfkd
+# account1.deposit(1000)
+# account1.withdraw(400)
+# account1.withdraw(800)  # This withdrawal will fail due to insufficient funds
+# print(f"Final balance: ${account1.get_balance()}")
+# # hjfkd
+
+
+def division(a, b):
+    try:
+        return a // b
+    except ZeroDivisionError:
+        return "Division by zero is not allowed."
+    except TypeError:
+        return "Only numbers are allowed."
+    except Exception as e:
+        return f"An unexpected error occurred: {e}"
+
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_division(self):
+        res = division(10, 2)
+        self.assertEqual(res, 5)
+
+        res1 = division(10, 0)
+        self.assertEqual(res1, "Division by zero is not allowed.")
+        
+        self.assertEqual(division("10", 2), "Only numbers are allowed.")
+        self.assertEqual(division(10, "2"), "Only numbers are allowed.")
+        self.assertEqual(division("10", "2"), "Only numbers are allowed.")
+        # self.assertEqual(division(10, 2, 3), "An unexpected error occurred: division() takes 2 positional arguments but 3 were given")
+
+
+
+unittest.main()      
 
